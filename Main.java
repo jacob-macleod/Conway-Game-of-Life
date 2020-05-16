dcb462a0224a8bd2282da0a793366acd77554580
jacob-macleod <jacob.e.macleod@gmail.com>
2020-05-16 11:35:24 +0100

    Updated all files

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 class Main {
   public static void main(String[] args) {
     grid_class grid = new grid_class ();
-   }
+    grid.print_grid();
+    grid.game_logic();
+    System.out.println("new grid");
+    grid.print_grid();
+  } 
 }
+
--- grid_class.java
+++ grid_class.java
@@ -1,10 +1,17 @@
 public class grid_class {
   int[][] grid_data = {
-            {3, 3, 3, 3, 3, 3},
-            {3, 1, 0, 0, 0, 3}, 
-            {3, 0, 1, 0, 0, 3}, 
-            {3, 0, 0, 0, 0, 3}, 
-            {3, 3, 3, 3, 3, 3}
+            {3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3},
+            {3, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3}, 
+            {3, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3}, 
+            {3, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+            {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+            {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3}, 
+            {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+            {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+            {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3}, 
+            {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+            {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3}, 
+            {3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3}
   };
 
   //Used to calculate where live cells are in function<insert function name>
@@ -13,12 +20,50 @@ public class grid_class {
   int x;
   int y;
   int a;
+  int b;
+  int count;
   int live_cell_count = 0;
   
+
+  //Prints the game grid
+  public void print_grid() {
+    int count = 0;           
+    for (int[] innerArray: grid_data) {
+      for(int data: innerArray) {
+        System.out.print(data);
+        count++;
+        if (count > 21) {
+          System.out.println();
+          count = 0;
+        }
+      }
+    }
+  } 
+  
+
+
+  //The logic for the game
+  public void game_logic () {
+    for (int b=0;b<grid_data.length;b++) {
+      if (grid_data[b][b] != 3) {
+        if (check_neighbours(b, b) < 2 || check_neighbours(b, b) > 3) {
+          kill_cell(b, b);
+        } else if (check_neighbours(b, b) == 3) {
+          resurrect_cell(b, b);
+        }
+      }
+    }
+  }
+
   //'kill' cell in a given co-ordinate
   public void kill_cell (int i, int j) {
-    System.out.println(grid_data[i][j]);
     grid_data[i][j] = 0;
+  }
+
+
+  //'resurrect' cell in a given co-ordinate
+  public void resurrect_cell (int i, int j) {
+    grid_data[i][j] = 1;
   }
 
   //Check the number of adjacent live cells in a given coordinate
